8bac7a1cc9c6a46a6b730feb4b7722ad46ba8eac
Samuel Costa <samuel.costa@fpf.br>
2021-09-15 14:06:02 -0400

    Initial Commit

---
+++ Memorizer/MemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerApp.swift
+//  Memorizer
+//
+//  Created by Samuel Vieira on 15/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
