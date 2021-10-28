2f9f97fdd48e4064f51fbf38cf0751f02c0a0e96
Samuel Costa <samuel.costa@fpf.br>
2021-10-28 18:40:48 -0400

    Finishing the model

--- Memorizer/EmojisMemoryGame.swift
+++ Memorizer/EmojisMemoryGame.swift
@@ -8,7 +8,15 @@
 import SwiftUI
 
 class EmojisMemoryGame {
-    private(set) var model: MemoryGame<String>
+    static let emojis: [String] = ["🚂", "🚜", "🛵", "✈️", "🚗", "🚕", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🏍", "🚍", "🛩", "🚇", "🛸"]
+    
+    static func createCardContent() -> MemoryGame<String> {
+        MemoryGame(numbersOfPairsOfCards: 6) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    private(set) var model: MemoryGame<String> = createCardContent()
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
--- Memorizer/MemorizeGame.swift
+++ Memorizer/MemorizeGame.swift
@@ -10,13 +10,23 @@ import Foundation
 struct MemoryGame<CardContent> {
     var cards: Array<Card>
     
+    init(numbersOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        
+        for pairIndex in 0..<numbersOfPairsOfCards{
+            let content: CardContent = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
     func choose(_ card: Card) {
         
     }
     
     struct Card {
-        var isFacedUp: Bool
-        var isMatched: Bool
+        var isFacedUp: Bool = false
+        var isMatched: Bool = false
         var content: CardContent
     }
 }
